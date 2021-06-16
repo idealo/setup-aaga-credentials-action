fb0ceda789f4a2dbbd664b23026c4ad914bcf595
heiko.rothe <heiko.rothe@idealo.de>
2021-06-16 14:50:33 +0200

    feat: clean up aws config files afterwards
    For security reasons.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 import * as io from '@actions/io'
-import {AuthContext, AwsCreds, TranslatorClient} from './translator-client'
+import {AwsCreds, TranslatorClient} from './translator-client'
 import path from 'path'
 import * as fs from 'fs'
 
